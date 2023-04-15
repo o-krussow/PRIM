8052fa64b7d1ad83d7603eebc81f6617b55ae7a1
Sam Aldeguer <aldeguersam@gmail.com>
2023-04-14 22:00:54 -0500

    Finish very basic structure

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,7 @@ function secondLevel(beginValue)
 		document.getElementById("suggestResult").style.display = "none";
 		document.getElementById("predictResult").style.display = "none";
 		document.getElementById("compareResult").style.display = "none";
+		showData('nullSubmit');
 
 	}
 	if (beginValue == "suggest")
@@ -12,17 +13,55 @@ function secondLevel(beginValue)
 		document.getElementById("suggestResult").style.display = "inline-block";
 		document.getElementById("predictResult").style.display = "none";
 		document.getElementById("compareResult").style.display = "none";
+		showData('suggestSubmit');
 	}
 	if (beginValue == "predict")
 	{
 		document.getElementById("suggestResult").style.display = "none";
 		document.getElementById("predictResult").style.display = "inline-block";
 		document.getElementById("compareResult").style.display = "none";
+		document.getElementById("suggestData").style.display = "none";
+		document.getElementById("predictData").style.display = "none";
+		document.getElementById("compareData").style.display = "none";
 	}
 	if (beginValue == "compare")
 	{
 		document.getElementById("suggestResult").style.display = "none";
 		document.getElementById("predictResult").style.display = "none";
 		document.getElementById("compareResult").style.display = "inline-block";
+		document.getElementById("suggestData").style.display = "none";
+		document.getElementById("predictData").style.display = "none";
+		document.getElementById("compareData").style.display = "none";
+	}
+}
+
+function showData(whichData)
+{
+	if (whichData == 'nullSubmit')
+	{
+		document.getElementById("suggestData").style.display = "none";
+		document.getElementById("predictData").style.display = "none";
+		document.getElementById("predictData").style.display = "none";
+	}
+	if (whichData == 'suggestSubmit')
+	{
+		/* INSERT CODE TO GENERATE STOCK DATA FOR SUGGEST HERE */
+		document.getElementById("suggestData").style.display = "block";
+		document.getElementById("predictData").style.display = "none";
+		document.getElementById("compareData").style.display = "none";
+	}
+	if (whichData == 'predictSubmit')
+	{
+		/* INSERT CODE TO GENERATE STOCK DATA FOR PREDICT HERE */
+		document.getElementById("suggestData").style.display = "none";
+		document.getElementById("predictData").style.display = "block";
+		document.getElementById("compareData").style.display = "none";
+	}
+	if (whichData == 'compareSubmit')
+	{
+		/* INSERT CODE TO GENERATE STOCK DATA FOR COMPARE HERE */
+		document.getElementById("suggestData").style.display = "none";
+		document.getElementById("predictData").style.display = "none";
+		document.getElementById("compareData").style.display = "block";
 	}
 }
