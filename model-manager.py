af6a47aa52f456e34e66fbc57716d295aebacd51
Owen Krussow <okrus-dev@tuta.io>
2023-04-15 01:19:57 -0500

    more comments and added more function to model-manager.py

--- model-manager.py
+++ model-manager.py
@@ -3,33 +3,36 @@ import json
 
 class model_manager:
 
-
     def __init__(self):
         self.models = {} #Not going to make getters and setters, just easier to access this dictionary directly I think
+
+        ticker_files_path = "./pickled_models/" #adjust if you put pickled files in sub directory
+
         try:
             with open("trained-models.json") as f:
                 tickers = json.load()
 
             #depickle models for tickers
             for ticker in tickers: #Assuming pickled file name is just the ticker
-                self.models[ticker] = pickle.load(open(ticker, "r"))
+                self.models[ticker] = pickle.load(open(ticker_files_path+ticker, "rb"))
 
         except FileNotFoundError:
             with open("trained-models.json") as f:
                 json.dump(f, []) #just make empty json if it doesn't exist already, this doesn't really NEED to happen because commit_json will do it anyways but it's fine
 
 
-    def access_model(self, ticker_name):
+    def access_model(self, ticker_name): #I named this totally arbitrarily, I don't really know how we want to do this part
         #run inference/somehow call predict function in pierce's thing however he decides to make it work
-        continue
+        pass
 
     def add_model(self, ticker_name):
 
         #need to:
         #instantiate new model (model takes csv path and name)
+        #CSV will be uploaded from client to server, not sure yet where it'll pop up here yet. This function might have to be put inside the flask code, along with a lot of this class.
         #train the model
 
-        self.models[ticker_name] = "" #add trained model to dictionary
+        self.models[ticker_name] = "" #add trained model to dictionary, the "" is just a placeholder
 
         #pickle the new trained model
         pickle_model(ticker_name)
@@ -39,8 +42,8 @@ class model_manager:
     def pickle_model(self, ticker):
         model = self.models[ticker]
 
-        with open(ticker, "w+") as f:
-            pickle.dump(model, f)
+        with open(ticker_files_path+ticker, "wb") as f:
+            pickle.dump(model, f) #so we can access later
 
     def commit_json(self):
         with open("trained-models.json", "w") as f:
