f72359a4c5d13374199c7e288041e420bb67dc47
Sam Aldeguer <aldeguersam@gmail.com>
2023-04-14 21:28:36 -0500

    Make one line/organizational

--- frontend/index.js
+++ frontend/index.js
@@ -9,20 +9,20 @@ function secondLevel(beginValue)
 	}
 	if (beginValue == "suggest")
 	{
-		document.getElementById("suggestResult").style.display = "block";
+		document.getElementById("suggestResult").style.display = "inline-block";
 		document.getElementById("predictResult").style.display = "none";
 		document.getElementById("compareResult").style.display = "none";
 	}
 	if (beginValue == "predict")
 	{
 		document.getElementById("suggestResult").style.display = "none";
-		document.getElementById("predictResult").style.display = "block";
+		document.getElementById("predictResult").style.display = "inline-block";
 		document.getElementById("compareResult").style.display = "none";
 	}
 	if (beginValue == "compare")
 	{
 		document.getElementById("suggestResult").style.display = "none";
 		document.getElementById("predictResult").style.display = "none";
-		document.getElementById("compareResult").style.display = "block";
+		document.getElementById("compareResult").style.display = "inline-block";
 	}
 }
