2590ea646cd4bb221e60798f055acfdab12585a0
PierceLaneKU <93334033+PierceLaneKU@users.noreply.github.com>
2023-04-15 23:05:33 -0500

    Completed lstm.py!     - The lstm neural network is now complete, with the ability to       predict into the future, hypertune its parameters, and two       different ways to test the ability of the network.     - Also added Figure_1.png, an image to use during the demo of a good       looking prediction     - Also deleted a bunch of old, bad files.

--- lstm.py
+++ lstm.py
@@ -19,21 +19,23 @@ import os
 
 import time
 
-
 class Model:
     def __init__(self, data_file_name, time_span = 60):
+        #data_type = "Stock Prices", x_axis_label = "Date"):
+        #self._x_axis_label = x_axis_label
+        #self._data_type = data_type
         self._time_span = time_span
         self._batch_size = 32
         self._epochs = 100
         #open the data file
         self._df = pd.read_csv(data_file_name)
         self._df_train = self._df.iloc[:, :].values
-        _null_train_df, self._return_test_df = train_test_split(self._df_train, train_size=0.8, test_size=0.2, shuffle=False)
         self._df_train_unscaled = self._df.iloc[:, :].values
-
+        
         #scale the data to [0, 1]
         self._scaler = MinMaxScaler(feature_range = (0, 1))
-        self._df_train = self._scaler.fit_transform(self._df_train)
+        self._df_train = self._scaler.fit_transform(self._df_train_unscaled)
+
         self._create_model()
 
     def _create_model(self):
@@ -50,30 +52,27 @@ class Model:
         self._model = Sequential()
 
         #add an lstm layer
-        self._model.add(LSTM(units=1, return_sequences=True, input_shape=(self._features_set.shape[1], 1)))
+        self._model.add(LSTM(60, return_sequences=True,
+                             input_shape = (self._features_set.shape[1], 1)))
         #then add a dropout layer to prevent overfitting to training data
-        self._model.add(Dropout(0.2))
+        self._model.add(Dropout(0.4))
         
-        #add another couple lstm/droupout layers
-        self._model.add(LSTM(units=1, return_sequences=True))
-        self._model.add(Dropout(0.2))
-
-        self._model.add(LSTM(units=1, return_sequences=True))
-        self._model.add(Dropout(0.2))
-
-        self._model.add(LSTM(units=1))
-        self._model.add(Dropout(0.2))
+        #add another couple lstm/dropout layers
+        self._model.add(LSTM(30, dropout = 0.0))
+        self._model.add(Dropout(0.4))
 
         #now add a Dense layer that condenses the inputs into a single value output
-        self._model.add(Dense(units = 2))
-     
+        self._model.add(Dense(20, activation = 'relu'))
+        self._model.add(Dense(1, activation = 'sigmoid'))
+
         #now compile the model
         self._model.compile(optimizer = 'adam', loss = 'mean_squared_error')
         
         #save initial blank weights
-        self._model.save_weights('model.h5')
+        self._model.save_weights('model_blank.h5')
     
-    def train(self, epochs = 100, batch_size = 32):
+    def train(self, epochs = 100, batch_size = 32, save = False):
+
         self._batch_size = batch_size
         checkpoint_path = "training_1/cp.ckpt"
         checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -91,6 +90,7 @@ class Model:
                            start_from_epoch = 20,
                            verbose = 1,
                            restore_best_weights= True)
+
         self.history = History()
         result = self._model.fit(self._features_set, self._labels, 
                         epochs = epochs, batch_size = self._batch_size,
@@ -99,9 +99,16 @@ class Model:
                         callbacks=[model_checkpoint_callback, self.history, es])
         validation_loss = np.amin(result.history['val_loss'])
         self._model.load_weights(checkpoint_path)
-        return validation_loss
         
-    def hyperfit(self, epochs = [2], batch_sizes = [7, 14, 32]):
+        if save:
+            self._model.save_weights("model.h5")
+
+        return validation_loss
+
+    def load_weights(self, path_to_weights):
+        self._model.load_weights(path_to_weights)
+        
+    def hyperfit(self, epochs = [50, 100], batch_sizes = [7, 14, 32], save = False):
         #lower fitness value is better
         #(best fitting model, fitness value of best model)
         best_model = (None,  0)
@@ -113,140 +120,113 @@ class Model:
                 self._reset_weights()
 
         self._model = best_model[0]
-        print(best_model[1])
+        if save:
+            self._model.save_weights("model.h5")
 
         #return to model_manager for graphs
-        return (self._return_test_df, self._return_test_df, self.history.history['loss'], self.history.history['val_loss'])
+        return (self.history.history['loss'], self.history.history['val_loss'])
 
     def _reset_weights(self):
-        self._model.load_weights('model.h5')
-
-    def test(self):
-        pass
+        self._model.load_weights('model_blank.h5')
 
     def predict(self, futurecast):
-        predictions = []
-        for i in range(0, futurecast):
-            predictions.append(self._predict_next_timestep())
+        # generate the multi-step forecasts
+        data_future = []
 
-        self._df_train_unscaled = self._df.iloc[:, :].values
+        feature_set_pred = [self._df_train[-self._time_span:]]  # last observed input sequence
+        price_pred = [[self._df_train[-1]]]                         # last observed target value
+        feature_set_pred = np.array(feature_set_pred)
+        price_pred = np.array(price_pred)
+        for i in range(futurecast):
 
-        return predictions
+            # feed the last forecast back to the model as an input
+            feature_set_pred = np.append(feature_set_pred[:, 1:, :], price_pred.reshape(1, 1, 1), axis = 1)
 
-    def _predict_next_timestep(self):
-        test_inputs = self._df_train_unscaled[-self._time_span:]
-        test_inputs = test_inputs.reshape(-1, 1)
-        test_inputs = self._scaler.transform(test_inputs)
+            # generate the next forecast
+            price_pred = self._model.predict(feature_set_pred)
 
-        test_features = []
-        test_features.append(test_inputs[0:self._time_span, 0])
+            # save the forecast
+            data_future.append(price_pred.flatten()[0])
 
-        test_features = np.array(test_features)   
-        test_features = np.reshape(test_features, (test_features.shape[0], test_features.shape[1], 1))
+        # transform the forecasts back to the original scale
+        data_future = np.array(data_future).reshape(-1, 1)
+        data_future = self._scaler.inverse_transform(data_future)
 
-        prediction = self._model.predict(test_features, verbose = 0)
-        prediction = self._scaler.inverse_transform(prediction)
-        self._df_train_unscaled = np.append(self._df_train_unscaled, prediction)
+        data_future = data_future.flatten().tolist()
+        print(data_future)
+        return data_future
+
+    def test_30_days(self):
+        # generate the multi-step forecasts
+        data_future = []
+
+        feature_set_pred = [self._df_train[-self._time_span:]]  # last observed input sequence
+        price_pred = [[self._df_train[-1]]]                         # last observed target value
+        feature_set_pred = np.array(feature_set_pred)
+        price_pred = np.array(price_pred)
+        for i in range(30):
+
+            # feed the last forecast back to the model as an input
+            feature_set_pred = np.append(feature_set_pred[:, 1:, :], price_pred.reshape(1, 1, 1), axis = 1)
+
+            # generate the next forecast
+            price_pred = self._model.predict(feature_set_pred)
+
+            # save the forecast
+            data_future.append(price_pred.flatten()[0])
+
+        # transform the forecasts back to the original scale
+        data_future = np.array(data_future).reshape(-1, 1)
+        data_future = self._scaler.inverse_transform(data_future)
+
+        return data_future, self._df_train_unscaled[-30:]
+
+    def _test(self):
+        size = 300
+        x = np.zeros((size, 60, 1))
+        y = np.zeros((size,))
+        for i in range(size):
+            x[i] = self._df_train[i: 60+i]
+            y[i] = self._df_train[i]
         
-        return prediction[0][0]
+        temp = self._model.predict(x)
 
+        plt.figure(figsize=(10,6))
+        plt.plot(y, color='blue', label=f"Actual price")
+        plt.plot(temp, color='red', label=f'Predicted price')
+        plt.title(f'Stock Price Prediction')
+        plt.xlabel("Date")
+        plt.ylabel("Stock Price")
+        plt.legend()
+        plt.show()
+        
 def main():
-    """
-    ###TRAINING FUNCTION START###
-    apple_training_complete = pd.read_csv(r'AAPL_training.csv')
-    apple_training_processed = apple_training_complete.iloc[:, 1:2].values   
-    scaler = MinMaxScaler(feature_range = (0, 1))
-
-    apple_training_scaled = scaler.fit_transform(apple_training_processed)
-    
-    features_set = []
-    self._self._labels = []
-    for i in range(60, len(apple_training_scaled)):
-        features_set.append(apple_training_scaled[i-60:i, 0])
-        self._self._labels.append(apple_training_scaled[i, 0])
-
-    
-    features_set, self._self._labels = np.array(features_set), np.array(labels)
-    features_set = np.reshape(features_set, (features_set.shape[0], features_set.shape[1], 1))
-
-    #make the model
-    model = Sequential()
-    
-    #add an lstm layer
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(features_set.shape[1], 1)))
-    #then add a dropout layer to prevent overfitting to training data
-    model.add(Dropout(0.2))
-
-    #add another couple lstm/droupout layers
-    model.add(LSTM(units=50, return_sequences=True))
-    model.add(Dropout(0.2))
-
-    model.add(LSTM(units=50, return_sequences=True))
-    model.add(Dropout(0.2))
-
-    model.add(LSTM(units=50))
-    model.add(Dropout(0.2))
-
-    #now add a Dense layer that condenses the inputs into a single value output
-    model.add(Dense(units = 1))
-
-    #now compile the model
-    model.compile(optimizer = 'adam', loss = 'mean_squared_error')
- 
-    #now train the model using our features and label
-    model.fit(features_set, self._self._labels, epochs = 100, batch_size = 32)
-    ###TRAINING FUNCTION END###
-    
-    #now test the model
-    apple_testing_complete = pd.read_csv(r'AAPL_testing.csv')
-    apple_testing_processed = apple_testing_complete.iloc[:, 1:2].values
-
-    apple_total = pd.concat((apple_training_complete['Open'], apple_testing_complete['Open']), axis=0)
- 
-    test_inputs = apple_total[len(apple_total) - len(apple_testing_complete) - 60:].values
-
-    #reshape the test data like we did with the training data
-    test_inputs = test_inputs.reshape(-1,1)
-    test_inputs = scaler.transform(test_inputs)
-
-    test_features = []
-    for i in range(60, 80):
-        test_features.append(test_inputs[i-60:i, 0])
-    
-    test_features = np.array(test_features)
-    test_features = np.reshape(test_features, (test_features.shape[0], test_features.shape[1], 1))
-  
-    predictions = model.predict(test_features)
-
-    predictions = scaler.inverse_transform(predictions)
-
-    plt.figure(figsize=(10,6))
-    plt.plot(apple_testing_processed, color='blue', label='Actual Apple Stock Price')
-    plt.plot(predictions , color='red', label='Predicted Apple Stock Price')
-    plt.title('Apple Stock Price Prediction')
-    plt.xlabel('Date')
-    plt.ylabel('Apple Stock Price')
-    plt.legend()
-    plt.show()
-    """
     
     start_time = time.time()
+    
+    model = Model("FSKAX-10y-1d.csv")
+    
+    #model.train(50, 14, save = True)
+    model.hyperfit(save = True)
+    model.load_weights('model.h5')
+    
+    predictions = model.predict(25)
 
-    model = Model("BASMX-10y-1d.csv")
-    model.hyperfit()
-    predictions = model.predict(20)
+    for i in range(100):
+        predictions.insert(0, None)
 
-    print(f"Time in seconds: {time.time() - start_time}")
+    data = model._df_train_unscaled[-100:]
 
+    #model.test()
+    
     plt.figure(figsize=(10,6))
-    plt.plot(predictions, color='red', label='Predicted Stock Price')
-    plt.title('Stock Price Prediction')
-    plt.xlabel('Date')
-    plt.ylabel('Stock Price')
+    plt.plot(data, color='blue', label=f"Actual price")
+    plt.plot(predictions, color='red', label=f'Predicted price')
+    plt.title(f'Stock Price Prediction')
+    plt.xlabel("Date")
+    plt.ylabel("Stock Price")
     plt.legend()
     plt.show()
-
-    
 
 if __name__ == "__main__":
     main()
